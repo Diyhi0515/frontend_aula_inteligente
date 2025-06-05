f8a29c63cbccd5f878f617c208f82d7b6226053a
Helen Baldelomar <helendiyhana03@gmail.com>
2025-06-05 02:40:55 -0400

    Update axiosInstance.js

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://18.212.39.82:5000/api',
 });
 
 export default axiosInstance;
